c607345dec208bd51f453fc2c1d34ff5ffa415d0
Richard Pett <rich_pett@hotmail.com>
2014-05-06 20:41:34 +0100

    updating email to and from fields

--- form/free_settings.php
+++ form/free_settings.php
@@ -1,8 +1,8 @@
 <?php
 
-$email_to = "rich_pett@hotmail.com"; // your email address
+$email_to = "info@roberthayplanning.com"; // your email address
 $email_subject = "Holmbury Site Enqiry"; // email subject line
 $thankyou = "../frm_thankyou.php"; // thank you page
-$headers = "From: rich_pett@hotmail.com";
+$headers = "From: info@roberthayplanning.com";
 
 ?>
